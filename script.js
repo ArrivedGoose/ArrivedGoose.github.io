e92608917d0d9473747bca6d54b3085ddc80d11a
ArrivedGoose <cuttlefish802@gmail.com>
2022-02-17 16:56:56 -0500

    improved some stuff

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+var TextBox = document.getElementById("code-bank")
 function display() {
     if (document.getElementById("Python_Dropdown").value == "Screen-Cordinate-Finder")
         alert("Hello World");
+        document.getElementById("code-bank").style.display="block";
+        console.log("I tried")
 }
